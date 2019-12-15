8e86d9138a946367404f6ba5d2d7b1b6e7bee0b4
shinkeonkim <singun11@naver.com>
2019-12-15 15:38:20 +0900

    add peach_controller

---
+++ src/main/peach_controller.py
@@ -0,0 +1,34 @@
+import sqlite3
+import billboard
+import pytube
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import os
+import subprocess
+import threading
+import sys
+
+class peachData:
+    def __init__(self):
+        pass
+
+    def setPath(self):
+        pass
+
+    
+    def initChart(self):
+        pass
+    
+
+    def initChartDict(self):
+        pass
+
+    def initDirectory(self):
+        pass
+
+    def getDirectoryMusicDict(self):
+        pass
+
+    def downloadMusic(self , song, artist):
+        pass
